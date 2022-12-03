7412345d4adc7dfe429cae1129a47f371725349a
Denis_web_dev <denis13.2011@mail.ru>
2022-12-03 11:53:49 +0300

    initial commit

--- src/lr1/Example9.java
+++ src/lr1/Example9.java
@@ -5,16 +5,21 @@ import java.util.Scanner;
 public class Example9 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-
+        String d;
         System.out.println("Введите название месяца: ");
         String month = in.nextLine();
 
         System.out.println("Введите колличество дней: ");
-        float age = in.nextFloat();
+        int count = in.nextInt();
+        if (count == 31) {
+            d = "день";
+        }
+        else d = "дней";
 
 
-        System.out.println("Месяц: " + month);
-        System.out.println("Месяц содержит: " + age);
+
+        System.out.println("Месяц: " + month + "-" + count + " " + d);
+
         in.close();
 
     }
