40763c0f86a12a1ef23fa20e1710649402fa508a
Denis_web_dev <denis13.2011@mail.ru>
2022-12-03 21:24:15 +0300

    initial commit

--- src/lr1/Example10.java
+++ src/lr1/Example10.java
@@ -1,7 +1,17 @@
 package lr1;
 
+import java.util.Scanner;
+
 public class Example10 {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
 
+        System.out.println("Введите год рождения: ");
+        int year = in.nextInt();
+        int age = 2022 - year;
+
+        System.out.println("Возраст " + age);
+
+        in.close();
     }
 }
