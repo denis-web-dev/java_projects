1131e88885f1ea687e79386f9b949f6a837b0194
Denis_web_dev <denis13.2011@mail.ru>
2022-12-03 22:05:14 +0300

    initial commit

---
+++ src/lr1/Example14.java
@@ -0,0 +1,26 @@
+package lr1;
+
+import java.util.Scanner;
+
+public class Example14 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Введите число: ");
+        int a = in.nextInt();
+
+        int b = a - 1;
+
+        int c = a + 1;
+
+        int d = (int)Math.pow((a + b + c),2);
+
+
+
+
+        System.out.println(b + " " + a + " " + c + " " + d);
+
+        in.close();
+
+    }
+}
