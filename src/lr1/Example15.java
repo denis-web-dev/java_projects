9317ebcd877a2393545f93f643d3d3274ecbd170
Denis_web_dev <denis13.2011@mail.ru>
2022-12-03 22:10:41 +0300

    initial commit

---
+++ src/lr1/Example15.java
@@ -0,0 +1,26 @@
+package lr1;
+
+import java.util.Scanner;
+
+public class Example15 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Введите первое число: ");
+        int x = in.nextInt();
+
+
+        System.out.println("Введите второе число: ");
+        int y = in.nextInt();
+
+        int sum = x + y;
+        int diff = x -y;
+
+
+
+        System.out.println("Сумма двух чисел: " + sum + "," + " разность двух чисел: " + diff);
+
+        in.close();
+
+    }
+}
