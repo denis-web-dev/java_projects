bd34db84a57adfe25d900c5eaee3c31de8a84c2d
Denis_web_dev <denis13.2011@mail.ru>
2022-12-18 21:50:52 +0300

    initial commit lr3

--- src/lr3/Example3.java
+++ src/lr3/Example3.java
@@ -5,16 +5,38 @@ import java.util.Scanner;
 public class Example3 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        int x = in.nextInt();
         int num0 = 1;
         int num1 = 1;
         int num2;
         System.out.print(num0 + " " + num1 + " ");
-        for (int i = 3; i <= 11; i++){
-            num2 = num0 + num1;
+//        for (int i = 3; i <= x; i++){
+//            num2 = num0 + num1;
+//            System.out.print(num2 + " ");
+//            num0 = num1;
+//            num1 = num2;
+//        }
+
+//        int i = 3;
+//        do {
+//            num2 = num0 + num1;
+//            System.out.print(num2 + " ");
+//            num0 = num1;
+//            num1 = num2;
+//            i++;
+//        }
+//        while (i <= x);
+
+            int i = 3;
+            while (i <= x){
+                num2 = num0 + num1;
             System.out.print(num2 + " ");
             num0 = num1;
             num1 = num2;
-        }
+            i++;
+            }
+
         System.out.println();
+
     }
 }
