28e1031deaf6ee8bb1478fca170037882ce424d2
Denis_web_dev <denis13.2011@mail.ru>
2022-12-24 00:57:59 +0300

    initial commit lr4

---
+++ src/lr4/Example2.java
@@ -0,0 +1,16 @@
+package lr4;
+
+public class Example2 {
+    public static void main(String[] args) {
+        int height = 10; // число столбцов которое необходимо вывести
+        int z = 0; // переменная необходима для вывода служебного сообщения о количестве символов в строке;
+        for (int i = 0; i < height; i++) {
+            System.out.print("номер строки: " + i + " ");
+            for (int j = 0; j <= z; j++) {
+                System.out.print("+");
+            }
+            z = z + 1;
+            System.out.println(" Количество символов в строке " + z);
+        }
+    }
+}
