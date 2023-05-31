90c1e0ac5db93b21b7bb50c22c5c8a4f92bf303c
Denis_web_dev <denis13.2011@mail.ru>
2023-05-31 10:31:17 +0300

    initial commit lr10

---
+++ src/lr10_2/DeleteBook.java
@@ -0,0 +1,47 @@
+package lr10_2;
+
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Scanner;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class DeleteBook {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        JSONParser parser = new JSONParser();
+        Object obj = null;
+        try {
+            obj = parser
+                    .parse(new FileReader("C:\\Users\\HP\\IdeaProjects\\java_projects\\src\\lr10_2\\exemple-json.json"));
+        } catch (IOException | ParseException e) {
+            throw new RuntimeException(e);
+        }
+        JSONObject library =  (JSONObject) obj;
+        JSONArray books = (JSONArray) library.get("books");
+        System.out.println("Введите название книги, которую хотите удалить: ");
+        String title = in.nextLine();
+        Iterator iterator = books.iterator();
+        while (iterator.hasNext()) {
+            JSONObject book = (JSONObject) iterator.next();
+            if (title.equals(book.get("title"))) {
+                iterator.remove();
+            }
+        }
+        try(FileWriter file = new FileWriter("C:\\Users\\HP\\IdeaProjects\\java_projects\\src\\lr10_2\\exemple-json.json"))
+        {
+            file.write(library.toJSONString());
+            System.out.println("Книга "+title+" удалена.");
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+}
